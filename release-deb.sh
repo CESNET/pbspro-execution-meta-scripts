50cdda54e508b5fce5736dd8589d4d67176d43be
Václav Chlumský <chlumskyvaclav@gmail.com>
2024-02-28 11:33:22 +0100

    Version bump: 1.0-9

--- release-deb.sh
+++ release-deb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dpkg-deb -b debian openpbs-execution-meta-scripts_$(echo $(grep "Version:" debian/DEBIAN/control) | awk '{print $2}')_all.deb
+fakeroot dpkg-deb -b debian openpbs-execution-meta-scripts_$(echo $(grep "Version:" debian/DEBIAN/control) | awk '{print $2}')_all.deb
 #alien -r openpbs-execution-meta-scripts_$(echo $(grep "Version:" debian/DEBIAN/control) | awk '{print $2}')_all.deb --scripts -k
